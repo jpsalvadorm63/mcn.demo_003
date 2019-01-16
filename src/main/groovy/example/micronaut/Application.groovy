b6c50e78d02367e3183a79195e060cfed1f4a906
jpsalvadorm63 <jpsalvadorm@briza-soft.com>
2019-01-16 15:16:51 -0500

    Completed

--- src/main/groovy/example/micronaut/Application.groovy
+++ src/main/groovy/example/micronaut/Application.groovy
@@ -1,11 +1,31 @@
 package example.micronaut
 
+import io.micronaut.context.event.ApplicationEventListener
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
+import io.micronaut.runtime.server.event.ServerStartupEvent
 
 @CompileStatic
-class Application {
+class Application implements ApplicationEventListener<ServerStartupEvent> {
+
+    private final RegisterUseCase registerUseCase
+
+    Application(RegisterUseCase registerUseCase) {
+        this.registerUseCase = registerUseCase
+    }
+
     static void main(String[] args) {
         Micronaut.run(Application)
+    }
+
+    @Override
+    void onApplicationEvent(ServerStartupEvent event) {
+        try {
+            registerUseCase.register("harry@micronaut.example")
+            Thread.sleep(20000)
+            registerUseCase.register("ron@micronaut.example")
+        } catch (InterruptedException e) {
+            e.printStackTrace()
+        }
     }
 }
---
+++ src/main/groovy/example/micronaut/RegisterUseCase.groovy
@@ -0,0 +1,37 @@
+package example.micronaut
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.TaskScheduler
+import javax.inject.Named
+import javax.inject.Singleton
+import java.text.SimpleDateFormat
+import java.time.Duration
+
+@CompileStatic
+@Slf4j
+@Singleton
+class RegisterUseCase {
+
+    protected final TaskScheduler taskScheduler
+    protected final EmailUseCase emailUseCase
+
+    RegisterUseCase(
+            EmailUseCase emailUseCase,
+            @Named(TaskExecutors.SCHEDULED) TaskScheduler taskScheduler
+    ) {
+        this.emailUseCase = emailUseCase
+        this.taskScheduler = taskScheduler
+    }
+
+    void register(String email) {
+        log.info("saving {} at {}", email, new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date()))
+        scheduleFollowupEmail(email, "Welcome to Micronaut")
+    }
+
+    private void scheduleFollowupEmail(String email, String message) {
+        EmailTask task = new EmailTask(emailUseCase: emailUseCase, email: email, message: message)
+        taskScheduler.schedule(Duration.ofMinutes(1), task)
+    }
+}
